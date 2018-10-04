ea528311719fbb33762501d083acd48374969dda
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-10-04 16:40:20 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1255,7 +1255,28 @@ if (message.content === '!inv') {
   } 
  });
  
- 
+ client.on('message', message => {
+ if (true) {
+if (message.content === '!invite') {
+     message.author.send(' Bot Link |  https://discordapp.com/api/oauth2/authorize?client_id=480067910966968330&permissions=0&scope=bot ').catch(e => console.log(e.stack));
+
+   }
+  } 
+ });
+
+client.on('message', message => {
+    if (message.content === "!invite") {
+    let embed = new Discord.RichEmbed()
+ .setAuthor(message.author.username)
+ .setColor("#9B59B6")
+ .addField(" Done " , " |  Send to PM")
+    
+    
+    
+ message.channel.sendEmbed(embed);
+   }
+});
+
 
 client.on('message', message => {
     if (message.content === "!inv") {
