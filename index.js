f85d1bf07db5b1be6711ebc22ec60162f4973f09
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-10-19 22:01:11 +0400

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
 if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'move')) {
+if(message.content.startsWith('!move')) {
  if (message.member.hasPermission("MOVE_MEMBERS")) {
  if (message.mentions.users.size === 0) {
  return message.channel.send("``لاستخدام الأمر اكتب هذه الأمر : " +prefix+ "move [USER]``")
@@ -73,6 +73,8 @@ message.react("❌")
 }
  }
 });
+
+var prefix = "!";
 
 client.on("message", message => {
     if (message.author.bot || !message.guild) return; 
