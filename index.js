977c677c5bda5140ed799feac1ab36b83b4ac636
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-10-19 15:40:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.on("message", message => {
     var command = args[0].slice(prefix.length);
     switch(command) {
         case "rainbow" :
-        if (message.member.hasPermission("MANAGE_ROLES")) return message.reply("no no");
+        if (message.member.hasPermission("ADMINISTRATOR")) return message.reply("no no");
         message.guild.createRole({name : "rainbow", color : "RANDOM"}).then(r => {
             r.edit({color : "RANDOM"});
             suck[message.guild.id] = {role : r.id};
